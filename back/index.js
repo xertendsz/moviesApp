704c165a8c90e3046dffb01b481c2a460ca3fd41
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-11-01 23:01:09 +0100

    build

--- back/index.js
+++ back/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT
+const port = 3000
 
 app.use(express.json())
 app.use(cors())
